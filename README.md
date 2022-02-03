2f3258d6fe20a90760755493e610f90f50803237
Fabrício Júnio <j.fabricio6@gmail.com>
2022-02-03 07:52:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,150 @@
+# Sobre:
+Este bot tem o seu código aberto, de forma que qualquer pessoa pode vê-lo, fazer uma fork, ou updates.
+
+Desenvolvi esse bot primeiramente para conseguir ter uma noite de sono 😴🥱 sem precisar ficar acordando de madrugada para dar start nas lutas. Eu decidi publica-lo aqui para ajudar o pessoal que como eu tem 3 guerreiras ou menos (podendo ter upgrades futuros para quem tem mais personagens).
+Se este projeto conseguir salvar algumas horas de sono para você também, salva a grana da coca-cola 🥤
+ 
+### Paypal:
+[Donate:](https://www.paypal.com/donate/?business=HTKSK9AAKTRPQ&no_recurring=0&item_name=Just+another+computer+guy+looking+for+his+place+in+the+sun%2C+so+grateful+for+your+support.%0A%0AObrigado+pelo+apoio&currency_code=BRL)
+https://www.paypal.com/donate/?business=HTKSK9AAKTRPQ&no_recurring=0&item_name=Just+another+computer+guy+looking+for+his+place+in+the+sun%2C+so+grateful+for+your+support.%0A%0AObrigado+pelo+apoio&currency_code=BRL
+
+### Smart Chain Wallet(BUSD/BNB/LUS):
+#### 0x0141D5033068E05427e4cAf840ecF06d12c926D2
+
+## Aviso:
+
+#### O uso e aplicação deste projeto é de sua total responsabilidade, Não me responsabilizo por eventuais penalidades sofridas por quem usar o bot, use por sua própria conta e risco.
+
+# Instalação:
+### Baixe e instale o Python pelo [site](https://www.python.org/downloads/) ou pela [windows store](https://www.microsoft.com/p/python-37/9nj46sx7x90p?activetab=pivot:overviewtab).
+
+Se você baixar pelo site é importante marcar a opção para adicionar o
+python ao PATH:
+![Check Add python to PATH](https://github.com/fjcunha/lunarush-bot/raw/ee1b3890e67bc30e372359db9ae3feebc9c928d8/readme-images/path.png)
+
+### Realize o download do codigo no formato zip, e extraia o arquivo.
+
+### Copie o caminho até a pasta do bot:
+
+![caminho](https://github.com/fjcunha/lunarush-bot/raw/main/readme-images/address.png)
+
+### Abra o terminal.
+
+Aperte a tecla do windows + r e digite "cmd":
+
+![launch terminal](https://github.com/fjcunha/lunarush-bot/raw/main/readme-images/cmd.png)
+
+### Navegue até a pasta do bot:
+Digite o comando "cd" + caminho que você copiou:
+
+![cd](https://github.com/fjcunha/lunarush-bot/raw/main/readme-images/cd.png)
+
+### Instale as dependências:
+
+```
+pip install -r requirements.txt
+```
+
+  
+![pip](https://github.com/fjcunha/lunarush-bot/raw/main/readme-images/pip.png)
+
+### Pronto! Agora é só iniciar o bot com o comando
+
+```
+python3 index.py
+```
+
+![run](https://github.com/fjcunha/lunarush-bot/raw/main/readme-images/run.png)
+
+
+# Como usar?
+
+Abra o terminal, se ainda não tiver navegado para a pasta do bot dê novamente o comando
+
+```
+"cd" + caminho que você copiou
+```
+
+Para iniciar use o comando 
+
+```
+python3 index.py
+```
+
+Assim que ele iniciar ele vai começar tentando iniciar uma luta. Para que ele funcione é preciso que a janela do game esteja aparecendo na sua tela, e suas guerreiras já selecionadas nos 3 slots.
+Ele vai constantemente checar se você foi desconectado para realizar o login novamente.
+
+  ----------------
+
+## Como funciona?
+
+O bot não interage diretamente com o jogo, ele somente tira print da tela do
+game para encontrar os botões e simula movimentos do mouse, isso faz com que
+diferenciar o bot de um humano seja muito difícil.
+
+## Ajustando o bot
+
+### Por que uns ajustes podem ser necessários?
+
+O bot usa reconhecimento de imagem para tomar decisões e movimentar o mouse e
+clicar nos lugares certos. 
+Ele realiza isso comparando uma imagem de exemplo com um screenshot da tela do
+computador.
+Este método está sujeito a inconsistências devido a diferenças na resolução da
+sua tela e de como o jogo é renderizado no seu computador comparado com o
+meu(o que usei para pegar as imagens exemplo).
+É provável que o bot não funcione 100% logo de cara, e que você precise fazer
+alguns ajustes aqui ou ali.
+
+### Quais sao os problemas?
+
+**Falso negativo** - O bot deveria reconhecer uma imagem, por exemplo, o botão de
+mandar para trabalhar, mas não reconheceu a imagem na screenshot.
+
+**Falso positivo** - O bot pensa que reconheceu a imagem que está procurando em um
+lugar em que esta imagem não aparece.
+
+Aqui tem uma [lista](#alguns-comportamentos-que-podem-indicar-um-falso-positivo-ou-negativo) de alguns problemas que podem ser ocasionados por falsos
+positivos e negativos.
+
+Para resolver estes problemas existem duas possibilidades, a regulagem do
+parâmetro “threshold” no arquivo config.yaml ou a substituição da imagem de
+exemplo na pasta “targets” para uma tirada no seu próprio computador:
+
+  ### Threshold na config
+
+  O parâmetro “threshold” regula o quanto o bot precisa estar confiante para
+  considerar que encontrou a imagem que está procurando.
+  Este valor de 0 a 1 (0% a 100%).
+  Ex:
+
+  Um threshold de 0.1 é muito baixo, ele vai considerar que encontrou a imagem
+  que esta procurando em lugares que ela não está aparecendo ( falso positivo ).
+  O comportamento mais comum pra esse problema é o bot clicando em lugares
+  aleatórios pela tela. 
+
+
+  Um threshold de 0.99 ou 1 é muito alto, ele não vai encontrar a imagem que
+  está procurando, mesmo quando ela estiver aparecendo na tela. O comportamento
+  mais comum é ele simplesmente não mover o cursor para lugar nenhum, ou travar
+  no meio de um processo, como o de login.
+
+  ### Substituição da imagem na pasta targets
+
+  As imagens exemplo são armazenadas na pasta “targets”. Estas imagens foram
+  tiradas no meu computador e podem estar um pouco diferente da que aparece no
+  seu. Para substituir alguma imagem que não esta sendo reconhecida
+  propriamente, simplesmente encontre a imagem correspondente na pasta targets,
+  tire um screenshot da mesma área e substitua a imagem anterior. É importante
+  que a substituta tenha o mesmo nome, incluindo o .png.
+
+
+### Algumas configuraçoes podem ser mudadas no arquivo config.yaml, nao se esqueça de reiniciar o bot caso mude as configuraçoes.
+
+## Curtiu? Dê aquela fortalecida :)
+
+### Wallet:
+#### 0xbd06182D8360FB7AC1B05e871e56c76372510dDf
+### Paypal:
+[Donate](https://www.paypal.com/donate/?business=HTKSK9AAKTRPQ&no_recurring=0&item_name=Just+another+computer+guy+looking+for+his+place+in+the+sun%2C+so+grateful+for+your+support.%0A%0AObrigado+pelo+apoio&currency_code=BRL)
+
---
+++ index.py
@@ -0,0 +1,321 @@
+# -*- coding: utf-8 -*-    
+from email.mime import image
+from src.logger import logger, loggerBossFight, loggerBossHunt, loggerMapClicked
+from cv2 import cv2
+from os import listdir
+from random import randint
+from random import random
+import numpy as np
+import mss
+import pyautogui
+import time
+import sys
+import yaml
+
+# Load config file.
+stream = open("config.yaml", 'r')
+c = yaml.safe_load(stream)
+ct = c['threshold']
+pause = c['time_intervals']['interval_between_moviments']
+pyautogui.PAUSE = pause
+
+welcome_text = """
+░░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░
+░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░
+░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░
+░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░
+░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░
+░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░
+░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░
+░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░
+░░████████████░░░█████████████████░░░░░░
+=========================================================================
+========== 💰 Have I helped you in any way? All I ask is a tip! 🧾 ======
+========== ✨ Faça sua boa ação de hoje, manda aquela gorjeta! 😊 =======
+=========================================================================
+======================== vvv LUS BUSD BNB vvv =========================
+============== 0x0141D5033068E05427e4cAf840ecF06d12c926D2 ===============
+=========================================================================
+===== xxxxxxxx ======
+=========================================================================
+
+>>---> Press ctrl + c to kill the bot.
+
+>>---> Some configs can be found in the config.yaml file."""
+
+
+
+
+
+
+
+def addRandomness(n, randomn_factor_size=None):
+    """Returns n with randomness
+    Parameters:
+        n (int): A decimal integer
+        randomn_factor_size (int): The maximum value+- of randomness that will be
+            added to n
+
+    Returns:
+        int: n with randomness
+    """
+
+    if randomn_factor_size is None:
+        randomness_percentage = 0.1
+        randomn_factor_size = randomness_percentage * n
+
+    random_factor = 2 * random() * randomn_factor_size
+    if random_factor > 5:
+        random_factor = 5
+    without_average_random_factor = n - randomn_factor_size
+    randomized_n = int(without_average_random_factor + random_factor)
+    # logger('{} with randomness -> {}'.format(int(n), randomized_n))
+    return int(randomized_n)
+
+def moveToWithRandomness(x,y,t):
+    pyautogui.moveTo(addRandomness(x,10),addRandomness(y,10),t+random()/2)
+
+
+def remove_suffix(input_string, suffix):
+    """Returns the input_string without the suffix"""
+
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
+def load_images(dir_path='./targets/'):
+    """ Programatically loads all images of dir_path as a key:value where the
+        key is the file name without the .png suffix
+
+    Returns:
+        dict: dictionary containing the loaded images as key:value pairs.
+    """
+
+    file_names = listdir(dir_path)
+    targets = {}
+    for file in file_names:
+        path = 'targets/' + file
+        targets[remove_suffix(file, '.png')] = cv2.imread(path)
+
+    return targets
+
+def clickBtn(img, timeout=3, threshold = ct['default']):
+    """Search for img in the screen, if found moves the cursor over it and clicks.
+    Parameters:
+        img: The image that will be used as an template to find where to click.
+        timeout (int): Time in seconds that it will keep looking for the img before returning with fail
+        threshold(float): How confident the bot needs to be to click the buttons (values from 0 to 1)
+    """
+
+    print("\n");
+    logger(None, progress_indicator=True)
+    start = time.time()
+    has_timed_out = False
+    while(not has_timed_out):
+        matches = positions(img, threshold=threshold)
+
+        if(len(matches)==0):
+            has_timed_out = time.time()-start > timeout
+            continue
+
+        x,y,w,h = matches[0]
+        pos_click_x = x+w/2
+        pos_click_y = y+h/2
+        moveToWithRandomness(pos_click_x,pos_click_y,1)
+        pyautogui.click()
+        return True
+
+    return False
+
+def printSreen():
+    with mss.mss() as sct:
+        monitor = sct.monitors[0]
+        sct_img = np.array(sct.grab(monitor))
+        # The screen part to capture
+        # monitor = {"top": 160, "left": 160, "width": 1000, "height": 135}
+
+        # Grab the data
+        return sct_img[:,:,:3]
+
+def positions(target, threshold=ct['default'],img = None):
+    if img is None:
+        img = printSreen()
+    result = cv2.matchTemplate(img,target,cv2.TM_CCOEFF_NORMED)
+    w = target.shape[1]
+    h = target.shape[0]
+
+    yloc, xloc = np.where(result >= threshold)
+
+
+    rectangles = []
+    for (x, y) in zip(xloc, yloc):
+        rectangles.append([int(x), int(y), int(w), int(h)])
+        rectangles.append([int(x), int(y), int(w), int(h)])
+
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+    return rectangles
+
+def isHome(hero, buttons):
+    y = hero[1]
+
+    for (_,button_y,_,button_h) in buttons:
+        isBelow = y < (button_y + button_h)
+        isAbove = y > (button_y - button_h)
+        if isBelow and isAbove:
+            # if send-home button exists, the hero is not home
+            return False
+    return True
+
+def isWorking(bar, buttons):
+    y = bar[1]
+
+    for (_,button_y,_,button_h) in buttons:
+        isBelow = y < (button_y + button_h)
+        isAbove = y > (button_y - button_h)
+        if isBelow and isAbove:
+            return False
+    return True
+
+def goToHeroes():
+    if clickBtn(images['go-back-arrow']):
+        global login_attempts
+        login_attempts = 0
+
+    #TODO tirar o sleep quando colocar o pulling
+    time.sleep(1)
+    clickBtn(images['hero-icon'])
+    time.sleep(randint(1,3))
+
+def refreshHeroesPositions():
+    logger('🔃 Refreshing Heroes Positions')
+    clickBtn(images['go-back-arrow'])
+    clickBtn(images['boss_hunt_icon'])
+
+    # time.sleep(3)
+    # clickBtn(images['boss_hunt_icon'])
+
+def login():
+    global login_attempts
+    logger('😿 Checking if game has disconnected')
+
+    if login_attempts > 3:
+        logger('🔃 Too many login attempts, refreshing')
+        login_attempts = 0
+        pyautogui.hotkey('ctrl','f5')
+        return
+
+    if clickBtn(images['connect-wallet'], timeout = 10):
+        logger('🎉 Connect wallet button detected, logging in!')
+        login_attempts = login_attempts + 1
+        #TODO mto ele da erro e poco o botao n abre
+        # time.sleep(10)
+
+    if clickBtn(images['select-wallet-2'], timeout=8):
+        # sometimes the sign popup appears imediately
+        login_attempts = login_attempts + 1
+        # print('sign button clicked')
+        # print('{} login attempt'.format(login_attempts))
+        if clickBtn(images['boss_home'], timeout = 15):
+            # print('sucessfully login, treasure hunt btn clicked')
+            login_attempts = 0
+        return
+        # click ok button
+
+    if not clickBtn(images['select-wallet-1-no-hover'], ):
+        if clickBtn(images['select-wallet-1-hover'], threshold = ct['select_wallet_buttons'] ):
+            pass
+            # o ideal era que ele alternasse entre checar cada um dos 2 por um tempo 
+            # print('sleep in case there is no metamask text removed')
+            # time.sleep(20)
+    else:
+        pass
+        # print('sleep in case there is no metamask text removed')
+        # time.sleep(20)
+
+    if clickBtn(images['select-wallet-2'], timeout = 20):
+        login_attempts = login_attempts + 1
+        # print('sign button clicked')
+        # print('{} login attempt'.format(login_attempts))
+        # time.sleep(25)
+        if clickBtn(images['boss_home'], timeout=25):
+            # print('sucessfully login, boss hunt btn on home clicked')
+            login_attempts = 0
+        # time.sleep(15)
+
+def main():
+    """Main execution setup and loop"""
+    # ==Setup==
+    global hero_clicks
+    global login_attempts
+    global last_log_is_progress
+    hero_clicks = 0
+    login_attempts = 0
+    last_log_is_progress = False
+
+    global images
+    images = load_images()
+
+    print(welcome_text)
+    time.sleep(7)
+    t = c['time_intervals']
+
+    last = {
+    "login" : 0,
+    "heroes" : 0,
+    "new_map" : 0,
+    "check_for_captcha" : 0,
+    "refresh_heroes" : 0,
+    "boss_hunt": 0,
+    "tap": 0,
+    }
+    # =========
+
+    while True:
+        now = time.time()
+
+        if now - last["login"] > addRandomness(t['check_for_login'] * 60):
+            sys.stdout.flush()
+            last["login"] = now
+            login()
+
+        if now - last["boss_hunt"] > addRandomness(t['boss_hunt'] * 60):
+
+            if clickBtn(images['boss_hunt']):
+                last["boss_hunt"] = now
+
+                loggerBossHunt()
+                time.sleep(60)
+                if(clickBtn(images['vs_icon'])):
+                    loggerBossFight()
+
+
+        if now - last["refresh_heroes"] > addRandomness( t['refresh_heroes_positions'] * 10):
+            last["refresh_heroes"] = now
+            refreshHeroesPositions()
+
+        if now - last["tap"] > 3:
+            last["tap"] = now
+            clickBtn(images['luna_icon'])
+            clickBtn(images['tap_to_open'])
+
+        sys.stdout.flush()
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+
+
+
+    main()
+
+
+# colocar o botao em pt
+# soh resetar posiçoes se n tiver clickado em newmap em x segundos
+
+
